890c47a860915230f157c9d2b0ecc6270263917f
Rifat Alam <rrifat.alam.93@gmail.com>
2022-05-16 21:41:09 +0600

    Add ant table and fix some issues

--- pages/api/slack-slash.js
+++ pages/api/slack-slash.js
@@ -1,32 +1,30 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import supabase from "./supabase";
 
 export default async function handler(req, res) {
   const { user_name, text } = req.body;
+  if (!["on", "off"].includes(text)) {
+    res.status(500);
+  }
+
   let { data: member, error } = await supabase
     .from("members")
     .select("*")
     .eq("user_name", user_name);
 
-  console.log("RESPONSE =>>", { member, error });
-
   if (member && !error) {
     const { data, error } = await supabase
       .from("members")
-      .upsert({ user_name, have_meal: text === "on" ? true : false });
-    // .eq("user_name", user_name);
-    console.log("INSERT & UPDATE =>>", { data, error });
+      .update({ have_meal: text === "on" ? true : false })
+      .eq("user_name", user_name);
+    error && res.status(500).json(error);
   } else if (member.length === 0 && !error) {
     const { data, error } = await supabase
       .from("members")
       .insert([{ user_name, have_meal: text === "on" ? true : false }]);
-
-    console.log("UPDATE =>>", { data, error });
+    error && res.status(500).json(error);
   } else {
-    console.log("ERROR =>>", error);
-    res.status(400).json(error);
+    res.status(500).json(error);
   }
-
   res.status(200).json({
     blocks: [
       {
